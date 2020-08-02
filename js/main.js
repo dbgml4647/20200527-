543abeabd1c1b7d5e9cb1dd58343c08e44b6ff08
Yuhee <dbgml4647@naver.com>
2020-08-03 02:45:08 +0900

    자바스크립트 BOM 실습

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-window.alert("자바 스크립트 실험")
+//javascript 실험 
+
+
+//alert 
+//window.alert("이것은 자바 스크립트 실험입니다")
+
+
+
+//confirm 
+/*
+if ( confirm('확인을 누르면 ok, 취소는 누르면 종료'))
+{
+    alert("OK!!")
+}
+else
+{
+
+    alert("END!!")
+}
+*/
+
+//prompt
+
+function promptfunc(){
+    
+    if(prompt('이름은 무엇인가요?') === "유희")
+    {
+        alert("이름이 정확해요!")
+    }
+    else
+    {
+        alert("이 사기꾼!")
+    }
+
+}
