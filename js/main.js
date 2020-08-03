f3ff951f517b8d068e70cd00645448b7b3e11c7b
Yuhee <dbgml4647@naver.com>
2020-08-03 19:40:25 +0900

    모달 실습 - 부가 버튼 만들었음

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function promptfunc(){
 
 }
 
-function zoomIn(event) {
+/*function zoomIn(event) {
     event.target.style.transform = "scale(1.5)";
     event.target.style.zIndex = 0;
     event.target.style.transition = "all 0.3s";
@@ -48,6 +48,41 @@ function zoomIn(event) {
     event.target.style.zIndex = 0;
     event.target.style.transition = "all 0.3s";
   }
+*/
+
+console.log(location.href)
+console.log(location.toString())
+
+/*location.reload()*/
+
+
+   // Get the modal
+   var modal = document.getElementById('myModal');
+ 
+   // Get the button that opens the modal
+   var btn = document.getElementById("myBtn");
+
+   // Get the <span> element that closes the modal
+   var span = document.getElementsByClassName("close")[0];                                          
+
+   // When the user clicks on the button, open the modal 
+   btn.onclick = function() {
+       modal.style.display = "block";
+   }
+
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+       modal.style.display = "none";
+   }
+
+   // When the user clicks anywhere outside of the modal, close it
+   window.onclick = function(event) {
+       if (event.target == modal) {
+           modal.style.display = "none";
+       }
+   }
+
+
 
 
 
