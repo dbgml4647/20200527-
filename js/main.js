80c5bcf4e5ec7a40c8bc746926506cc0dfbcf84f
Yuhee <dbgml4647@naver.com>
2020-08-03 23:37:01 +0900

    타임라인 호버 효과

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ function promptfunc(){
 
 }
 
-/*function zoomIn(event) {
+function zoomIn(event) {
     event.target.style.transform = "scale(1.5)";
     event.target.style.zIndex = 0;
     event.target.style.transition = "all 0.3s";
@@ -48,7 +48,48 @@ function promptfunc(){
     event.target.style.zIndex = 0;
     event.target.style.transition = "all 0.3s";
   }
-*/
+
+
+function onchangecolor(event){
+
+    document.body.style.background= pink;
+
+
+
+}
+
+
+
+
+
+
+function hover_in(event)
+{
+
+event.target.style.opacity=0.5;
+event.target.style.transition = "all 0.3s";
+document.style.display= "block"
+
+
+
+}
+
+
+function hover_out(event)
+{
+
+event.target.style.opacity=1;
+event.target.style.transition = "all 0.3s"
+
+
+
+}
+
+
+
+
+
+
 
 console.log(location.href)
 console.log(location.toString())
@@ -56,31 +97,6 @@ console.log(location.toString())
 /*location.reload()*/
 
 
-   // Get the modal
-   var modal = document.getElementById('myModal');
- 
-   // Get the button that opens the modal
-   var btn = document.getElementById("myBtn");
-
-   // Get the <span> element that closes the modal
-   var span = document.getElementsByClassName("close")[0];                                          
-
-   // When the user clicks on the button, open the modal 
-   btn.onclick = function() {
-       modal.style.display = "block";
-   }
-
-   // When the user clicks on <span> (x), close the modal
-   span.onclick = function() {
-       modal.style.display = "none";
-   }
-
-   // When the user clicks anywhere outside of the modal, close it
-   window.onclick = function(event) {
-       if (event.target == modal) {
-           modal.style.display = "none";
-       }
-   }
 
 
 
