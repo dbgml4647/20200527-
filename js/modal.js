02020da72c2e0e28b70e189fc35a6d9c28258ec5
Yuhee <dbgml4647@naver.com>
2020-08-14 18:46:23 +0900

    각 요소별 modal 추가하기

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,50 @@
 
-   // Get the modal
-   var modal = document.getElementById('myModal_2');
- 
-   // Get the button that opens the modal
+//여기서 프로젝트 아이디를 뽑아야한다 
+
+
+function reply_click(clicked_id)
+{
+    event.target.style.opacity=1;
+    event.target.style.zIndex = 0;
+    event.target.style.transition = "all 0.1s";
+
+    //alert(clicked_id);
+    var click_img = clicked_id;
+
+    //alert(click_img);
+
+     // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById(click_img);
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+
    
-    var img = document.getElementById("project_img_2");
-
-
-   // Get the <span> element that closes the modal
-   var span = document.getElementsByClassName("close_2")[0];     
-   
-   
+  
+}
 
 
 
-   // When the user clicks on the button, open the modal 
-   img.onclick = function() {
-       modal.style.display = "block";
-   }
-
-   // When the user clicks on <span> (x), close the modal
-   span.onclick = function() {
-       modal.style.display = "none";
-   }
-
-   // When the user clicks anywhere outside of the modal, close it
-   window.onclick = function(event) {
-       if (event.target == modal) {
-           modal.style.display = "none";
-       }
-   }
 
